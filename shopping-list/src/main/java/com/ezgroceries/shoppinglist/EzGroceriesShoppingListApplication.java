ccba0d88807d6a88619570f2c0de372f598192c5
Filip Branders <filip.branders@kbc.be>
2022-10-27 10:27:31 +0200

    lab-01-spring-boot

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplication.java
@@ -1,4 +1,4 @@
-package com.ezgroceries.shoppinglist.shoppinglist;
+package com.ezgroceries.shoppinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- shopping-list/src/test/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplicationTests.java
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ezgroceries.shoppinglist.shoppinglist;
+package com.ezgroceries.shoppinglist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
