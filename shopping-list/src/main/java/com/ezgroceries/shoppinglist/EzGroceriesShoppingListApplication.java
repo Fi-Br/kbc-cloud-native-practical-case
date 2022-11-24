f35c2c967a2f71de05f8609fb4ad40280317593f
Filip Branders <filip.branders@kbc.be>
2022-11-24 11:30:41 +0100

    lab-03-external-api

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/EzGroceriesShoppingListApplication.java
@@ -2,10 +2,12 @@ package com.ezgroceries.shoppinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EntityScan("ezgroceries.shoppinglist")
 public class EzGroceriesShoppingListApplication {
 
     public static void main(String[] args) {
